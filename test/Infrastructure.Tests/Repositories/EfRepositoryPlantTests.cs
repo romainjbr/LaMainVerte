99e570545054264552554e2cdfa634e454b01173
romainjbr <romainjbrafai@gmail.com>
2025-12-16 19:06:32 +0100

    Add unit tests in EfRepositoryPlantsTests

--- test/Infrastructure.Tests/Repositories/EfRepositoryPlantTests.cs
+++ test/Infrastructure.Tests/Repositories/EfRepositoryPlantTests.cs
@@ -21,6 +21,8 @@ public class EfRepositoryPlantTests
         _repo = new EfRepository<Plant>(db);
     }
     
+    #region  GetIdByAsync
+
     [Fact]
     public async Task AddAndGetById_Success_ReturnsCorrectPlant()
     {
@@ -45,4 +47,63 @@ public class EfRepositoryPlantTests
         Assert.Equal(WaterFrequency.Weekly, result.WaterFrequency);
         Assert.Equal("https://example.com/monstera.jpg", result.ImageUrl);
     }
+
+    #endregion 
+
+    #region  DeleteAsync
+
+    [Fact]
+    public async Task DeleteAsync_PlantFound_RemovePlant()
+    {
+        var plant = new Plant
+        {
+            Id = Guid.NewGuid(),
+            Name = "Aloe",
+            Species = "Aloe vera",
+            Location = "Bedroom",
+            WaterFrequency = WaterFrequency.Weekly
+        };
+
+        await _repo.AddAsync(plant, CancellationToken.None);
+
+        Assert.NotNull(await _repo.GetByIdAsync(plant.Id, CancellationToken.None));
+
+        await _repo.DeleteAsync(plant, CancellationToken.None);
+
+        var result = await _repo.GetByIdAsync(plant.Id, CancellationToken.None);
+        Assert.Null(result);
+    }
+
+    #endregion
+
+    #region UpdateAsync
+    [Fact]
+    public async Task UpdateAsync_Success_PersistsChanges()
+    {
+        var plant = new Plant
+        {
+            Id = Guid.NewGuid(),
+            Name = "Plantix",
+            Species = "Nephrolepis exaltata",
+            Location = "Hallway",
+            WaterFrequency = WaterFrequency.Weekly
+        };
+
+        await _repo.AddAsync(plant, CancellationToken.None);
+
+        plant.Location = "Bathroom";
+        plant.WaterFrequency = WaterFrequency.Daily;
+        plant.ImageUrl = "https://example.com/plantix.png";
+
+        await _repo.UpdateAsync(plant, CancellationToken.None);
+
+        var reloaded = await _repo.GetByIdAsync(plant.Id, CancellationToken.None);
+
+        Assert.NotNull(reloaded);
+        Assert.Equal("Bathroom", reloaded!.Location);
+        Assert.Equal(WaterFrequency.Daily, reloaded.WaterFrequency);
+        Assert.Equal("https://example.com/plantix.png", reloaded.ImageUrl);
+    }  
+
+    #endregion
 }
