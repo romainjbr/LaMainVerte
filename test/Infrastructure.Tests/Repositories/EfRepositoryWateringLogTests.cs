4cb45cba39939556da42be88cda68f3648a343b3
romainjbr <romainjbrafai@gmail.com>
2025-12-17 15:26:00 +0100

    Implement EfRepositoryWateringLogTests

---
+++ test/Infrastructure.Tests/Repositories/EfRepositoryWateringLogTests.cs
@@ -0,0 +1,63 @@
+using Core.Entities;
+using Core.Enums;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Tests.Repositories;
+
+public class EfRepositoryWateringLogTests
+{
+    public EfRepository<WateringLog> _repo;
+
+    public EfRepositoryWateringLogTests()
+    {
+        var options = new DbContextOptionsBuilder<PlantDbContext>().UseSqlite("Filename=:memory:").Options;
+
+        var db = new PlantDbContext(options);
+        db.Database.OpenConnection();
+        db.Database.EnsureCreated();      
+
+        _repo = new EfRepository<WateringLog>(db);
+    }
+
+    public static Plant GetPlant(Guid? id) => new Plant
+    {
+        Id = id ?? Guid.NewGuid(),
+        Name = "Monstera",
+        Species = "Monstera deliciosa",
+        Location = "Living room",
+        WaterFrequency = WaterFrequency.Weekly,
+        ImageUrl = "https://example.com/monstera.jpg"
+    };
+
+    public static WateringLog GetWateringLog(Guid? id, Guid plantId, Plant plant) => new WateringLog
+    {
+        Id = id ?? Guid.NewGuid(),
+        PlantId = plantId,
+        Plant = plant,
+        Date = DateTime.UtcNow       
+    };
+    
+    #region  GetIdByAsync
+
+    [Fact]
+    public async Task AddAndGetById_Success_ReturnsCorrectPlant()
+    {
+        var plantId = Guid.NewGuid();
+        var wateringLogId = Guid.NewGuid();
+
+        var plant = GetPlant(plantId);
+        var wateringLog = GetWateringLog(wateringLogId, plantId, plant);
+
+        await _repo.AddAsync(wateringLog, CancellationToken.None);
+
+        var result = await _repo.GetByIdAsync(wateringLog.Id, CancellationToken.None);
+
+        Assert.NotNull(result);
+        Assert.Equal(wateringLogId, result.Id);
+        Assert.Equal(plantId, result.PlantId);
+    }
+
+    #endregion
+}
