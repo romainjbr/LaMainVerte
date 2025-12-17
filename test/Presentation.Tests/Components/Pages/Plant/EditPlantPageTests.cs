2f9e94759e8a3e257fbc79b13fd1ada0dd859d0a
romainjbr <romainjbrafai@gmail.com>
2025-12-17 14:21:03 +0100

    Implement CallsPage_WithPlantID_DisplaysPlantName() in EditPlantPageTests

--- test/Presentation.Tests/Components/Pages/Plant/EditPlantPageTests.cs
+++ test/Presentation.Tests/Components/Pages/Plant/EditPlantPageTests.cs
@@ -1,8 +1,10 @@
 using Bunit;
 using Core.Dtos;
+using Core.Enums;
 using Core.Interface.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using Presentation.Components.Forms;
 using Presentation.Components.Pages.Plant;
 
 namespace Presentation.Tests.Components.Pages;
@@ -33,4 +35,36 @@ public class EditPlantPageTests : BunitContext
 
         Assert.Contains("Loading...", page.Markup);
     }
+
+    [Fact]
+    public void CallsPage_WithPlantID_DisplaysPlantName()
+    {
+        var plantId = Guid.NewGuid();
+
+        var plantDto = new PlantReadDto
+        (
+            plantId,
+            "Monstera",
+            "Monstera deliciosa",
+            "Living Room",
+            DateTime.UtcNow,
+            WaterFrequency.Weekly,
+            "monstera.png",
+            default,
+            default
+        );
+
+        var tcs = new TaskCompletionSource<PlantReadDto?>();
+
+        _svc.Setup(s => s.GetPlantByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(plantDto);
+
+        Services.AddSingleton(_svc.Object);
+        Services.AddSingleton(_imgSvc.Object);
+
+        var page = Render<EditPlant>(parameters => parameters.Add(p => p.Id, plantId));
+
+        Assert.Contains("Monstera", page.Markup);
+    }
+
 }
