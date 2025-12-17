38a6a1edea0a1bad25988ae0dbc97752d35eba81
romainjbr <romainjbrafai@gmail.com>
2025-12-17 14:25:43 +0100

    Remove IPlantImageService Mock from PlantsPageTests

--- test/Presentation.Tests/Components/Pages/Plant/PlantsPageTests.cs
+++ test/Presentation.Tests/Components/Pages/Plant/PlantsPageTests.cs
@@ -10,19 +10,16 @@ namespace Presentation.Tests.Components.Pages;
 public class PlantsPageTests : BunitContext
 {
     private readonly Mock<IPlantService> _svc;
-    private readonly Mock<IPlantImageService> _imgSvc;
 
     public PlantsPageTests()
     {
         _svc = new Mock<IPlantService>();
-        _imgSvc = new Mock<IPlantImageService>();
     }
 
     [Fact]
     public void CallsPage_InitiallyDisplaysLoading()
     {
         Services.AddSingleton(_svc.Object);
-        Services.AddSingleton(_imgSvc.Object);
 
         var page = Render<Plants>();
 
