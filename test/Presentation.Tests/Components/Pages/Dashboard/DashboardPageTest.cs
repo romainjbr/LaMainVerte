7dede4c83232c44c9065a1fcc63a5e808bb29661
romainjbr <romainjbrafai@gmail.com>
2025-12-17 15:15:19 +0100

    Implement CallsPage_ReturnsList_DisplaysListCount() test in DashboardPageTests

--- test/Presentation.Tests/Components/Pages/Dashboard/DashboardPageTest.cs
+++ test/Presentation.Tests/Components/Pages/Dashboard/DashboardPageTest.cs
@@ -1,5 +1,6 @@
 using Bunit;
 using Core.Dtos;
+using Core.Entities;
 using Core.Enums;
 using Core.Interface.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -34,5 +35,27 @@ public class DashboardPageTests : BunitContext
         var page = Render<Dashboard>();
 
         Assert.Contains("Loading...", page.Markup);
+    }
+
+    [Fact]
+    public void CallsPage_ReturnsList_DisplaysListCount()
+    {
+        var plantList = new List<PlantReadDto>
+        {
+            new (Guid.NewGuid(), "Monsterosa", "Monsterosa deliciosa", "Living Room", DateTime.Now, WaterFrequency.Daily, "", default, default),
+            new (Guid.NewGuid(), "Snaky", "Snake plant", "Bedoom", DateTime.Now, WaterFrequency.Weekly, "", default, default), 
+        };
+
+        _plantSvc.Setup(s => s.GetAllPlantsAsync(It.IsAny<CancellationToken>())).ReturnsAsync(plantList);
+        _plantSvc.Setup(s => s.GetPlantsNeedingWater(It.IsAny<CancellationToken>())).ReturnsAsync(new List<PlantReadDto>());
+        _wateringLogSvc.Setup(s => s.GetRecentAsync(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(new List<WateringLogReadDto>());
+
+        Services.AddSingleton(_plantSvc.Object);
+        Services.AddSingleton(_wateringLogSvc.Object);
+                
+        var page = Render<Dashboard>();
+
+        Assert.Contains("Total plants", page.Markup);
+        Assert.Contains($"{plantList.Count}", page.Markup);
     }
 }
