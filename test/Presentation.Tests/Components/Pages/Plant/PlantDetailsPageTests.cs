53c4781be7d5f528a6add6deec23076778b180e8
romainjbr <romainjbrafai@gmail.com>
2025-12-17 14:41:21 +0100

    Implement CallsPage_Found_DisplayPlantProperties() test

--- test/Presentation.Tests/Components/Pages/Plant/PlantDetailsPageTests.cs
+++ test/Presentation.Tests/Components/Pages/Plant/PlantDetailsPageTests.cs
@@ -1,5 +1,6 @@
 using Bunit;
 using Core.Dtos;
+using Core.Enums;
 using Core.Interface.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -30,5 +31,37 @@ public class PlantDetailsPageTests : BunitContext
         var page = Render<PlantDetails>(parameters => parameters.Add(p => p.Id, Guid.NewGuid()));
 
         Assert.Contains("Plant not found!", page.Markup);
+    }
+
+    [Fact]
+    public void CallsPage_Found_DisplayPlantProperties()
+    {
+        var plantId = Guid.NewGuid();
+
+        var plantDto = new PlantReadDto
+        (
+            plantId,
+            "Monstera",
+            "Monstera deliciosa",
+            "Living Room",
+            DateTime.UtcNow,
+            WaterFrequency.Weekly,
+            "monstera.png",
+            default,
+            default
+        );
+        
+        _plantSvc.Setup(s => s.GetPlantByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(plantDto);
+
+        Services.AddSingleton(_plantSvc.Object);
+        Services.AddSingleton(_WateringLogsvc.Object);
+
+        var page = Render<PlantDetails>(parameters => parameters.Add(p => p.Id, plantId));
+
+        Assert.Contains("Monstera", page.Markup);
+        Assert.Contains("Monstera deliciosa", page.Markup);
+        Assert.Contains("Living Room", page.Markup);
+        Assert.Contains("monstera.png", page.Markup);
     }
 }
